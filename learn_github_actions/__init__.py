e912bf2a98a483f4d6b7c6e1f59b7ae8d8404313
Jason Hall <jdhall75@gmail.com>
2025-10-16 22:22:56 -0400

    Adds python package for testing scm tags

---
+++ learn_github_actions/__init__.py
@@ -0,0 +1,3 @@
+"""Learn Github Actions package"""
+
+__version__ = "0.0.1"
---
+++ learn_github_actions/__main__.py
@@ -0,0 +1,7 @@
+"""Main runnable for learn-github-actions"""
+
+from .main import main
+
+
+if __name__ == "__main__":
+    main()
